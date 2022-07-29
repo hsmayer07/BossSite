2bb63adbc23765da6a450565d91d39a139fa90e5
Mahad Faruqi <mahadfaruqi@pal-nat186-108-184.itap.purdue.edu>
2022-07-29 14:07:15 -0400

    suggestions

--- pages/suggestions.py
+++ pages/suggestions.py
@@ -1,10 +1,14 @@
 import dash
-from dash import html, dcc
+from dash import Dash, html, dcc, callback, Input
 
 dash.register_page(__name__)
 
-layout = html.Div(children=[
-    html.H1(children='This is our suggestions page'),
-
-
+layout = html.Div([
+    dcc.Textarea(
+        id = 'suggestions-box',
+        value = 'Type your suggestions here.',
+        style={'width': '100%', 'height': 300},
+    ),
+    ##html.Button('Submit', id='textarea-state-example-button', n_clicks=0),
+    html.Div(id='suggestions-box', style={'whiteSpace': 'pre-line'})
 ])
