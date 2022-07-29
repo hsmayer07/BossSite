400bcaa1b693244a64a6ca162e7364b9c6b31826
Mahad Faruqi <mahadfaruqi@pal-nat186-108-184.itap.purdue.edu>
2022-07-29 16:28:03 -0400

    fuck me harder daddy

--- pages/suggestions.py
+++ pages/suggestions.py
@@ -1,21 +1,40 @@
 import dash
+import smtplib
+from dash.dependencies import Input, Output, State
 from dash import Dash, html, dcc, callback, Input
 
-dash.register_page(__name__)
-
-<<<<<<< HEAD
+dash.register_page(__name__,)
 layout = html.Div([
     dcc.Textarea(
-        id = 'suggestions-box',
+        id = 'suggestions_box',
         value = 'Type your suggestions here.',
         style={'width': '100%', 'height': 300},
-    ),
-    ##html.Button('Submit', id='textarea-state-example-button', n_clicks=0),
-    html.Div(id='suggestions-box', style={'whiteSpace': 'pre-line'})
-=======
-layout = html.Div(children=[
-    html.H1(children='This is our suggestions page'),
-    dcc.Link(html.Button("Return home"), href="/home", refresh=True)
+        ),
 
->>>>>>> d896c346346d01356c5ff9945c171a477579ddb7
-])
+    html.Button('Submit', id='submit_button', n_clicks=0),
+    #html.Div(id='suggestions_box_output', style={'whiteSpace': 'pre-line'})
+    ])
+
+@callback(
+        #Output('suggestions_box_output', 'children'),
+        Output('suggestions_box', "value"),
+        Input('submit_button', 'n_clicks'),
+        State('suggestions_box', 'value')
+        )
+
+def update_output(n_clicks, value):
+    if n_clicks > 0:
+
+        with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+            smtp.ehlo()
+            smtp.starttls()
+            smtp.ehlo()
+
+            subject = 'New BOSS suggestion'
+            smtp.login('mahadkhalid4955@gmail.com','etmwfqvkoeeaeval')
+            msg = f'Subject: {subject}\n\n{value}'
+            smtp.sendmail('mahadkhalid4955@gmail.com', 'mahadkhalid4955@gmail.com', msg)
+            return 'Thanks for adding a suggestion!'
+
+
+
